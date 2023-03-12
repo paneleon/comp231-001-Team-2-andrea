78834c378304e35ba7b0c05af7e3c5496376fa84
AndreaJDS <andrea_jolene@outlook.com>
2023-03-12 17:58:55 -0400

    test

---
+++ hotel-system/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() { 
+  return (
+    <>
+      <Router>
+         <Routes>
+          <Route index element= {<Home/>}/>
+          
+        </Routes>
+      </Router>
+      
+    </>
+  );
+}
+
+export default App;
+// https://www.youtube.com/watch?v=MpQbwtSiZ7E
