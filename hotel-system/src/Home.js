f9997e749025410095403aaffb4b1f6fe992daf2
AndreaJDS <andrea_jolene@outlook.com>
2023-03-26 22:36:57 -0400

    added hotel name

--- hotel-system/src/Home.js
+++ hotel-system/src/Home.js
@@ -11,7 +11,7 @@ function Home() {
         <>
         <Nav/>
         <div className="hero">
-            <h1 className="title">Hotel Name</h1>
+            <h1 className="title">Grand Hotel</h1>
             <p className="description">Welcome to our beautiful hotel located in the heart of the city. We offer luxurious accommodations and exceptional service to make your stay unforgettable.</p>
         </div>
         <div className="room-section">
@@ -19,7 +19,7 @@ function Home() {
             <p className="room-section-description">We offer a variety of room types to suit your needs, from cozy single rooms to spacious suites. All of our rooms are equipped with high-speed internet, flat-screen TVs, and premium bedding for a comfortable night's sleep.</p>
         </div>
         <footer>
-            <p>&copy; 2023 Hotel Name. All rights reserved.</p>
+            <p>&copy; 2023 Grand Hotel. All rights reserved.</p>
         </footer>
         </>
     )
