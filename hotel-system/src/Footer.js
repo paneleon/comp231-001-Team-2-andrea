f29e5102a2ebf2b23b2e02ab881005cbaf073ef9
AndreaJDS <andrea_jolene@outlook.com>
2023-04-14 21:03:35 -0400

    added footer component

---
+++ hotel-system/src/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <>
+     <footer style={{position: "fixed", bottom: 0, width: "100%"}}>
+        <p>&copy; 2023 Grand Hotel. All rights reserved.</p>
+    </footer>
+    </>
+  );
+}
+
+export default Footer;
