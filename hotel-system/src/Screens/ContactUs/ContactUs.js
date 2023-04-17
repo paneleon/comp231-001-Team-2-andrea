4882f0ab57a43cc64e637fe1c07b2c7fb11fb273
AndreaJDS <andrea_jolene@outlook.com>
2023-04-17 00:39:08 -0400

    updated css for contact us page

--- hotel-system/src/Screens/ContactUs/ContactUs.js
+++ hotel-system/src/Screens/ContactUs/ContactUs.js
@@ -1,9 +1,6 @@
-import React, {useState} from 'react';
-import Nav from '../../Components/Nav/Nav';
-import { useNavigate } from 'react-router-dom';
-import Footer from '../../Components/Footer/Footer';
 import './ContactUs.css';
-
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 
 function ContactUs() {
@@ -20,49 +17,55 @@ function ContactUs() {
         navigate("/");
     }
 
+
     return (
         <>
-        <Nav/>
-        <form className='contactform' onSubmit={handleSubmit} style={{border: "1px solid black", padding: "20px"}}>
-            <div>
-                <h2>Contact Us</h2>
-                <p>Email: abc@gmail.com</p>
-                <p>Phone: +14372773737</p>
-                <p>Or fill in the form and we will get back to you.</p>
+            <div className='contactus-main-container'>
+                <div className='contactus-header-container'>
+                    <h2>Contact Us</h2>
+                </div>
+                <div className='contactus-contactform-container'>
+                    <form className='contactform' onSubmit={handleSubmit}>
+                        <div>
+
+                            <p>Email: abc@gmail.com</p>
+                            <p>Phone: +14372773737</p>
+                            <p>Or fill in the form and we will get back to you.</p>
+                        </div>
+                        <div className='contactdetails'>
+                            <label htmlFor="name">Name:</label>
+                            <input
+                                type="text"
+                                id="name"
+                                value={name}
+                                onChange={(e) => setName(e.target.value)}
+                                required
+                            />
+                        </div>
+                        <div className='contactdetails'>
+                            <label htmlFor="email">Email:</label>
+                            <input
+                                type="email"
+                                id="email"
+                                value={email}
+                                onChange={(e) => setEmail(e.target.value)}
+                                required
+                            />
+                        </div>
+                        <div className='contactdetails'>
+                            <label htmlFor="inquiries">Inquiries:</label>
+                            <textarea
+                                id="inquiries"
+                                value={inquiries}
+                                onChange={(e) => setInquiries(e.target.value)}
+                                required
+                            />
+                        </div>
+                        <button className='contactform-submit-button' type="submit">Submit</button>
+                    </form>
+                </div>
             </div>
-            <div className='contactdetails'>
-            <label htmlFor="name">Name:</label>
-            <input
-                type="text"
-                id="name"
-                value={name}
-                onChange={(e) => setName(e.target.value)}
-                required
-            />
-            </div>
-            <div className='contactdetails'>
-            <label htmlFor="email">Email:</label>
-            <input
-                type="email"
-                id="email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                required
-            />
-            </div>
-            <div className='contactdetails'>
-            <label htmlFor="inquiries">Inquiries:</label>
-            <textarea
-                id="inquiries"
-                value={inquiries}
-                onChange={(e) => setInquiries(e.target.value)}
-                required
-            />
-            </div>
-            <button type="submit" className="btn btn-primary">Submit</button>
-        </form>
-        
-        <Footer/>
+
         </>
     )
 }
